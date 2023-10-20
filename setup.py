77010518429bfd05d6185420b9827fae45344fca
Andy Daubner <contact@andydaubner.com>
2023-10-20 18:36:55 +0200

    added package data

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@ from setuptools import setup
 setup(
     name="mysql-table-creator",
     version="0.0.1",
+    packages=["mysql_table_creator"],
+    package_data={
+        "mysql_table_creator": ["sql_literals.json"]
+    },
+    include_package_data=True,
     description="A package to create and append rows to a MySQL table",
     url="https://github.com/adaubner/mysql-table-creator",
     author="Andy Daubner, Che Hang Ng",
     license="MIT",
-    packages=["mysql_table_creator"],
     install_requires=["pyodbc"],
 )
